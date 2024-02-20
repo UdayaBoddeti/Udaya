b6b1da70ce3f74b20d1420c58260a10c98fe70a5
UdayaBoddeti <158701248+UdayaBoddeti@users.noreply.github.com>
2024-02-20 11:15:23 +0530

    rm PrimeNumber.c

--- PrimeNumber.c
+++ PrimeNumber.c
@@ -20,37 +20,37 @@ int inputvalidation(char value[])
 
 int isPrime(char value[])
 {
-	int counter, number, isprime = -1;
-	number = inputvalidation(value);
-	if (number == -1)
-	{
-		isprime = 0;
-	}	
-	if (number == 0 || number == 1)
-	{
-	isprime = 0;
-	}	
-	else if (number == 2 || number == 3)
-	{
-	isprime = 1;
-	}
-	else if(number % 2 == 0)
-	{
-	   isprime = 0;
-	}
-	else
-	{
-		int squareroot_value = sqrt(number);
-		isprime = 1;
-		for (counter = 3; counter <= squareroot_value; counter += 2)
-		{
-			if (number % counter == 0)
-			{
-			   isprime = 0;	
-	       break;
-			}	
-		}	
-	}
-	return isprime;
+    int counter, number, isprime = -1;
+    number = inputvalidation(value);
+    if (number == -1)
+    {
+    	isprime = 0;
+    }	
+    if (number == 0 || number == 1)
+    {
+    isprime = 0;
+    }	
+    else if (number == 2 || number == 3)
+    {
+    isprime = 1;
+    }
+    else if(number % 2 == 0)
+    {
+       isprime = 0;
+    }
+    else
+    {
+    	int squareroot_value = sqrt(number);
+    	isprime = 1;
+    	for (counter = 3; counter <= squareroot_value; counter += 2)
+    	{
+    		if (number % counter == 0)
+    		{
+    		   isprime = 0;	
+           break;
+    		}	
+    	}	
+    }
+    return isprime;
 }
 
